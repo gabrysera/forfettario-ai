d1e52ce93cefa71ffc7b86cdbb8b6229811dacf9
gabrysera <76791545+gabrysera@users.noreply.github.com>
2026-09-05 14:14:56 +0300

    style: sort AA9/12 exports

--- app/documents/aa912/__init__.py
+++ app/documents/aa912/__init__.py
@@ -4,9 +4,9 @@ from .renderer import render_aa912
 from .template import SUPPORTED_TEMPLATE, validate_template
 
 __all__ = [
+    "SUPPORTED_TEMPLATE",
     "AA912Draft",
     "AA912OpeningProfile",
-    "SUPPORTED_TEMPLATE",
     "build_aa912_draft",
     "render_aa912",
     "validate_template",
